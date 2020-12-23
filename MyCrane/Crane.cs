57228dc064de436e73818f2347e242c093b64a32
vldnik <vldnik0404@gmail.com>
2020-12-23 14:05:18 +0200

    Начальный возврат модуля MyCrane

---
+++ MyCrane/Crane.cs
@@ -0,0 +1,86 @@
+﻿using System;
+namespace MyCrane
+{
+    public class Crane
+    {
+        bool On;
+        int Direction;
+        int CargoOnBoard;
+
+        public Crane()
+        {
+            On = false;
+            Direction = 0;
+            CargoOnBoard = 0;
+        }
+
+        public void ToOn()
+        {
+            if (On == true)
+            {
+                throw new InvalidOperationException("Crane already on!");
+            }
+            else
+            {
+                On = true;
+            }
+        }
+        public void ToOff()
+        {
+            if (On == false)
+            {
+                throw new InvalidOperationException("Crane already off!");
+            }
+            else
+            {
+                On = false;
+            }
+        }
+        public void GetCargo(int cargo)
+        {
+            if (On == true && cargo > 0 && cargo < 200)
+            {
+                CargoOnBoard += cargo;
+            }
+            else
+            {
+                throw new InvalidOperationException("Crane couldn`t get cargo!");
+            }
+        }
+        public void ToLeft()
+        {
+            if (On == false)
+            {
+                throw new InvalidOperationException("Turn on the crane!");
+            }
+            if (Direction == 0)
+            {
+                Direction = 270;
+            }
+            else
+            {
+                Direction -= 90;
+            }
+        }
+        public void ToRight()
+        {
+            if (On == false)
+            {
+                throw new InvalidOperationException("Turn on the crane!");
+            }
+            if (Direction == 270)
+            {
+                Direction = 0;
+            }
+            else
+            {
+                Direction += 90;
+            }
+        }
+        public string CraneNow()
+            {
+                return "Crane engine works = " + On + " ,now holds " + CargoOnBoard + " kg, and it`s direction on " + Direction + " degrees.";
+            }
+        
+    }
+}
---
+++ MyCrane/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace MyCrane
+{
+    class Program
+    {
+        public static void WriteToDo()
+        {
+            Console.WriteLine("Construction crane. What to do: ");
+            Console.WriteLine("1-Turn on.");
+            Console.WriteLine("2-Turn off.");
+            Console.WriteLine("3-Get the cargo");
+            Console.WriteLine("4-Turn to the right.");
+            Console.WriteLine("5-Turn to the left.");
+            Console.WriteLine("6-Check crane.");
+            Console.WriteLine("7-Exit.");
+
+        }
+
+        public static void PrintException(string massage)
+        {
+            Console.WriteLine(massage);
+        }
+
+        static void Main(string[] args)
+        {
+            bool isOpen = true;
+            Crane crane = new Crane();
+            WriteToDo();
+
+            while (isOpen)
+            {
+                int action = Convert.ToInt32(Console.ReadLine());
+                try
+                {
+                    switch (action)
+                    {
+                        case 1:
+                            crane.ToOn();
+                            Console.WriteLine(crane.CraneNow());
+                            break;
+
+                        case 2:
+                            crane.ToOff();
+                            Console.WriteLine(crane.CraneNow());
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Insert waight of your cargo.");
+                            int weight = Convert.ToInt32(Console.ReadLine());
+                            crane.GetCargo(weight);
+                            Console.WriteLine(crane.CraneNow());
+                            break;
+
+                        case 4:
+                            crane.ToRight();
+                            Console.WriteLine(crane.CraneNow());
+                            break;
+
+                        case 5:
+                            crane.ToLeft();
+                            Console.WriteLine(crane.CraneNow());
+                            break;
+
+                        case 6:
+                            Console.WriteLine(crane.CraneNow());
+                            break;
+
+                        case 7:
+                            isOpen = false;
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    WriteToDo();
+                }
+                catch (InvalidOperationException ex)
+                {
+                    PrintException(ex.Message);
+                }
+            }
+        }
+    }
+}
